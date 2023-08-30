7c88ddb46361de1666e4286a5084e3ec01c4b70d
jonBeach <jonb3ach@gmail.com>
2023-08-30 00:51:47 -0400

    Added a check to see if the number is equal to the target (for 0's)

--- Two Sum/Two Sum.py
+++ Two Sum/Two Sum.py
@@ -4,16 +4,16 @@ class Solution(object):
         #iterate through all the numbers
         #by count up to the number of numbers in nums array starting at 0
         for i in range(len(nums)):
-            #check if the current number is greater than the target
+            #check if the current number is greater or equal to the target
             #because you can't add up to the target number if one
             #of the numbers is already greater than it
-            if nums[i] < target:
+            if nums[i] <= target:
                 #range(start, stop, step) (where start and step are optional arguments)
                 #using the above arguments we start the second iterating because
                 #we can't use the same element twice
                 for k in range(i+1,len(nums)):
-                    #check if greater than target same as above
-                    if nums[k] < target:
+                    #check if greater or equal to the target same as above
+                    if nums[k] <= target:
                         #if the two current numbers equal the target
                         if nums[i] + nums[k] == target:
                             #return the current numbers
